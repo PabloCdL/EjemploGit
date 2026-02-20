63fe3c5eb38b498acf2f1a32560062b9791c9daa
PabloCdL <angelcoyoy07@gmail.com>
2026-02-20 11:11:06 -0600

    Usando Git Primer comentario de git

--- EjemploGit/Program.cs
+++ EjemploGit/Program.cs
@@ -1,7 +1,10 @@
-﻿Console.WriteLine("Primer ejemplo de git");
+﻿using System.Xml;
+
+Console.WriteLine("Primer ejemplo de git");
 
 Console.WriteLine("Usando Git");
 Console.WriteLine("Usando Git");
 Console.WriteLine("Usando Git");
 Console.WriteLine("Usando Git");
 Console.WriteLine("Usando Git");
+Console.WriteLine("Ahoria si colocando el comentario jaja");
